07275b07638061f1a20e15aaf3759187ce133d6d
Eubene Sa <eubene.sa@gmail.com>
2016-08-10 15:59:24 -0700

    Add code snippet for ES5 - Object.keys

---
+++ 0_2-Object_keys/code.js
@@ -0,0 +1,16 @@
+var numsObj = {
+  a: 1,
+  'b': 2,
+  c: 3
+};
+
+var numsObjKeys = Object.keys(numsObj);
+console.log(numsObjKeys); // ['a', 'b', 'c']
+
+numsObjKeys.map(function(key) {
+  numsObj[key] *= 2;
+});
+
+numsObjKeys.map(function(key) {
+  console.log(numsObj[key]); // 2 4 6
+});
