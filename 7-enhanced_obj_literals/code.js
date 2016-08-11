de5e2f9c7ba0764169b3adbb009a7cf805c63852
Eubene Sa <eubene.sa@gmail.com>
2016-08-11 13:37:38 -0700

    Add code snippet for ES2015 - Enhanced Object Literals

---
+++ 7-enhanced_obj_literals/code.js
@@ -0,0 +1,7 @@
+var addressObj = {
+  ['address_' + 1]: 'Euresa.io',
+  ['address_' + 2]: '123 Ocean Avenue W',
+  city: 'Midnight City'
+}
+
+console.log(addressObj); // { address_1: 'Euresa.io', address_2: '123 Ocean Avenue W', city: 'Midnight City' }
